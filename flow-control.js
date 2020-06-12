351f1d796f35f356c131e9f3d9bc8ffce8679979
Lola Martinez <12lolamartinez@gmail.com>
2020-06-12 21:48:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,17 +5,17 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  
+
 }
 
 function ageChecker(age) {
-  
+
 }
 
 function ternaryTeenager(age) {
-  
+
 }
 
 function switchAge(age) {
-  
+
 }
