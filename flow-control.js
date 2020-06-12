5f50ff94e4f063950a75c9e912ee7c2513f8d1c7
Lola Martinez <12lolamartinez@gmail.com>
2020-06-12 22:31:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,8 +29,8 @@ return (age >=13 && age <=19 ? "You are a teenager" : "You are not a teenager")
 function switchAge(age) {
 switch (true) {
 case age: (age >=13 && age <=19)
-return "You are a teenager!" 
-break; 
+return "You are a teenager!"
+break;
 default: return "You have an age"
 }
 }
