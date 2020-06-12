bf6cf02cbf056aa569a25f87a2d2b6a21086d1cf
Lola Martinez <12lolamartinez@gmail.com>
2020-06-12 21:21:50 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,9 +29,9 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch (age) {
     case age >=13 && age <=19:
-    return "You have an age";
+    return "You are a teenager";
       break;
     default:
-    return "You are a teenager";
+    return "You have an age";
   }
 }
